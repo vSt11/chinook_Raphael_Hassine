4bc89bbef0b89e23f66c2f91e1b3d92ad21115cd
Raphael Hassine <raphael.hassine@student.isae-supaero.fr>
2023-04-13 18:11:58 +0200

    Database sucessfully linked to dataset

--- chinook/disks/models.py
+++ chinook/disks/models.py
@@ -4,14 +4,14 @@ class Artist(models.Model):
     name = models.CharField(max_length=100)
     
 class Album(models.Model):
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, null=True)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     
 class Track(models.Model):
-    album = models.ForeignKey(Album, on_delete=models.CASCADE, null=True)
-    name = models.CharField(max_length=100)
-    composer = models.CharField(max_length=100)
-    milliseconds = models.FloatField()
-    bytes = models.PositiveIntegerField()
-    unitPrice = models.DecimalField(max_digits=5, decimal_places=2)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    composer = models.CharField(max_length=100, null=True, blank=True)
+    milliseconds = models.IntegerField()
+    bytes = models.IntegerField()
+    unitPrice = models.DecimalField(max_digits=10, decimal_places=2)
 
