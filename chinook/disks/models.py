e83b37e2956deafd2413109497066be63eadc9c7
Raphael Hassine <raphael.hassine@student.isae-supaero.fr>
2023-04-13 17:41:17 +0200

    Adding disks app, configuring the route, initializing db

--- chinook/disks/models.py
+++ chinook/disks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    
+class Album(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=100)
+    
+class Track(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=100)
+    composer = models.CharField(max_length=100)
+    milliseconds = models.FloatField()
+    bytes = models.PositiveIntegerField()
+    unitPrice = models.DecimalField(max_digits=5, decimal_places=2)
+
